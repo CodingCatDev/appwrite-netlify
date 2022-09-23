9d6b05b8b6b525717f915654a07ccbd410d66d88
brittneypostma <bdesignedwebdev@gmail.com>
2022-09-23 16:31:16 -0400

    updated graph, still missing tokens

--- lib/netlifyGraph/index.d.ts
+++ lib/netlifyGraph/index.d.ts
@@ -52,6 +52,54 @@ errors?: Array<GraphQLError>;
       options?: NetlifyGraphFunctionOptions
     ): Promise<ExampleQuery>;
 
+export type SpotifySearchTracksInput = {
+ "query": string
+};
+    
+    export type SpotifySearchTracks = {
+  /**
+  * Any data from the function will be returned here
+  */
+data: /** No fields, named fragments, or inline fragments found */ Record<string, unknown>;
+  /**
+  * Any errors from the function will be returned here
+  */
+errors?: Array<GraphQLError>;
+};
+    
+    /**
+     * Search for matching Spotify tracks. Note you can edit this query to also search for artists
+     */
+    export function fetchSpotifySearchTracks(
+      variables: SpotifySearchTracksInput,
+      options?: NetlifyGraphFunctionOptions
+    ): Promise<SpotifySearchTracks>;
+
+
+    export type SpotifyAboutMeQuery = {
+  /**
+  * Any data from the function will be returned here
+  */
+data: {
+  me: /** No fields, named fragments, or inline fragments found */ Record<string, unknown>;
+};
+  /**
+  * Any errors from the function will be returned here
+  */
+errors?: Array<GraphQLError>;
+};
+    
+    /**
+     * Find the currently logged-in Spotify user's email, name, profile image, etc.
+     */
+    export function fetchSpotifyAboutMeQuery(
+      /**
+      * Pass `{}` as no variables are defined for this function.
+      */
+      variables: Record<string, never>,
+      options?: NetlifyGraphFunctionOptions
+    ): Promise<SpotifyAboutMeQuery>;
+
 
     export type SpotifyAboutMeQuery = {
   /**
@@ -82,6 +130,14 @@ errors?: Array<GraphQLError>;
     * 
     */
     fetchExampleQuery: typeof fetchExampleQuery,
+  /**
+    * Search for matching Spotify tracks. Note you can edit this query to also search for artists
+    */
+    fetchSpotifySearchTracks: typeof fetchSpotifySearchTracks,
+  /**
+    * Find the currently logged-in Spotify user's email, name, profile image, etc.
+    */
+    fetchSpotifyAboutMeQuery: typeof fetchSpotifyAboutMeQuery,
   /**
     * Find the currently logged-in Spotify user's email, name, profile image, etc.
     */
---
+++ pages/api/SpotifySearchTracks.js
@@ -0,0 +1,57 @@
+
+const NetlifyGraph = require("../../lib/netlifyGraph");
+
+exports.handler = async (req, res) => {
+  // By default, all API calls use no authentication
+  let accessToken = null;
+
+  //// If you want to use the client's accessToken when making API calls on the user's behalf:
+  // accessToken = req.headers["authorization"]?.split(" ")[1];
+
+  //// If you want to use the API with your own access token:
+  // accessToken = process.env.NETLIFY_GRAPH_TOKEN;
+      
+  const eventBodyJson = req.body || {};
+
+  const query = typeof req.query?.query === 'string' ? req.query?.query : req.query?.query[0];
+
+  if (query === undefined || query === null) {
+    return res.status(422).json({
+        errors: ["You must supply parameters for: `query`"],
+    });
+  }
+
+  const { errors, data } = await NetlifyGraph.fetchSpotifySearchTracks({ query: query }, {accessToken: accessToken}); 
+
+  if (errors) {
+    console.error(JSON.stringify(errors, null, 2));
+  }
+
+  console.log(JSON.stringify(data, null, 2));
+
+  res.setHeader("Content-Type", "application/json");
+
+  return res.status(200).json({
+    errors, data
+  });
+};
+
+exports.default = exports.handler;
+
+/** 
+ * Client-side invocations:
+ * Call your Netlify function from the browser with this helper:
+ */
+
+/**
+async function fetchSpotifySearchTracks(params) {
+  const {query} = params || {};
+  const resp = await fetch(`/api/SpotifySearchTracks?query=${query}`, {
+    method: "GET"
+  });
+
+  const text = await resp.text();
+
+  return JSON.parse(text);
+}
+*/
